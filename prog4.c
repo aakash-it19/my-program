31add335e8a976377216cc462a9813f222950a8f
aakash-it19 <aakash.it19@bitsathy.ac.in>
2019-10-11 11:46:33 +0530

    first commit

---
+++ prog4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+        int a,b,speed;
+        printf("enter the speed1:");
+        scanf("%d",&a);
+        printf("enter the speed2:");
+        scanf("%d",&b);
+        speed=(a+b)/2;
+        printf("the average speed travelled by the person is %d km/hr",speed);
+}
